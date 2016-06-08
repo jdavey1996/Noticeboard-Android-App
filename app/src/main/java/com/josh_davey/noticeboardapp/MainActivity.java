217629778f01a984a454942c3d268983d410d909
Josh Davey <jdavey1996@gmail.com>
2016-06-08 19:34:49 +0100

    Activity links
    Added links between the main activity (Login menu) and the register
    activity. Also added a button to cancel the registration and return to
    the main activity (Login menu).

--- app/src/main/java/com/josh_davey/noticeboardapp/MainActivity.java
+++ app/src/main/java/com/josh_davey/noticeboardapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.josh_davey.noticeboardapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button register;
+
+        register = (Button)findViewById(R.id.regActivityBtn);
+        register.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent startRegister = new Intent(MainActivity.this, Register.class);
+                startActivity(startRegister);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/josh_davey/noticeboardapp/Register.java
+++ app/src/main/java/com/josh_davey/noticeboardapp/Register.java
@@ -1,13 +1,30 @@
 package com.josh_davey.noticeboardapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
-public class Register extends Activity {
+public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+
+
+        Button cancelRegister;
+
+        cancelRegister = (Button)findViewById(R.id.cancelRegBtn);
+        cancelRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent cancelRegister = new Intent(Register.this, MainActivity.class);
+                startActivity(cancelRegister);
+                finish();
+            }
+        });
+
     }
 }
