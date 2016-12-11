2aa4d97aedc9fb177e7a5f063f55a30aefb67920
Josh Davey <jdavey1996@gmail.com>
2016-12-11 20:56:22 +0000

    Fixed listview alignment issue
    Prior to this commit, if posts varied in length, the swipe layout for
    that post would not match the post height. To resolve this issue, when
    posts are added to the listview, the heights are readjusted
    programatically, by getting the height of the post and updating the post
    swipe layout height.

--- app/src/main/java/com/josh_davey/noticeboardapp/PostsAdapter.java
+++ app/src/main/java/com/josh_davey/noticeboardapp/PostsAdapter.java
@@ -6,7 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.chauthai.swipereveallayout.SwipeRevealLayout;
@@ -58,15 +59,41 @@ public class PostsAdapter extends ArrayAdapter<Posts> {
         //Bind swipe layout with binder parameters set previously.
         binderHelper.bind(swipeLayout, filtered.getPostNum());
 
+        //Get both aspects of the post layout (list item and swipe layout view), and get image button.
+        final RelativeLayout deleteView = (RelativeLayout)convertView.findViewById(R.id.deletePostBtn);
+        final RelativeLayout listItemView = (RelativeLayout)convertView.findViewById(R.id.postlayout);
+        final ImageView deleteImg = (ImageView)convertView.findViewById(R.id.deleteImg);
 
         //If the logged in user matches the user of the post, a 'swipe left delete button' is visible, else it is hidden.
         if (loggedInUser.equals(filtered.getPostUser())) {
             //Unlock drag, allowing layout to be swiped out.
             swipeLayout.setLockDrag(false);
 
+            //Check if the heights match between the swipelayout for current list item and the current list item.
+            swipeLayout.post(new Runnable() {
+                @Override public void run() {
+                    //Get heights for each layout in posts.
+                    int h1 = deleteView.getMeasuredHeight();
+                    int h2 = listItemView.getMeasuredHeight();
+
+                    //If the swipe layout height is smaller than the list item height.
+                    if (h1 < h2)
+                    {
+                        //Update layout1 height to match layout2.
+                        ViewGroup.LayoutParams params = deleteView.getLayoutParams();
+                        params.height = h2;
+                        deleteView.setLayoutParams(params);
+
+                        //Center the delete button image after its container has been resized.
+                        RelativeLayout.LayoutParams params2 = (RelativeLayout.LayoutParams)deleteImg.getLayoutParams();
+                        params2.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+                        deleteImg.setLayoutParams(params2);
+                    }
+                }
+            });
+
             //OnClickListener for delete button. Runs asynctask to attempt to delete list item.
-            final FrameLayout deletePostButton = (FrameLayout) convertView.findViewById(R.id.deletePostBtn);
-            deletePostButton.setOnClickListener(new View.OnClickListener() {
+            deleteView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     //Run asynctask.
